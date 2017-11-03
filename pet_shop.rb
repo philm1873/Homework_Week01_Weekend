7db06eb55c90a1f2c2357864c3aa5bc7cb8710e6
Phil Mitchell <pm1873@gmail.com>
2017-11-03 12:13:10 +0000

    passed 5 & 6

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,11 +1,19 @@
-def pet_shop_name(shop)
- return shop[:name]
+def pet_shop_name(hash)
+ return hash[:name]
 end
 
-def total_cash(shop)
-  return shop[:admin][:total_cash]
+def total_cash(hash)
+  return hash[:admin][:total_cash]
 end
 
-def add_or_remove_cash(shop, amount)
-  return shop[:admin][:total_cash] += amount
+def add_or_remove_cash(hash, amount)
+  return hash[:admin][:total_cash] += amount
+end
+
+def pets_sold(hash)
+  return pets_sold = hash[:admin][:pets_sold]
+end
+
+def increase_pets_sold(hash1, amount)
+  return hash1[:admin][:pets_sold] += amount
 end
